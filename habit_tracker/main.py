0749e346b1b4fa55ac143da014c8091e42f76395
Rojen Dangol <rojendangol44@gmail.com>
2025-05-21 19:27:44 +0545

    create-graph

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,8 +1,11 @@
 import requests
+from datetime import datetime
+
 
 pixela_endpoint = "https://pixe.la/v1/users"
 USERNAME = "rojen123"
 TOKEN = "qwertyuiop"
+GRAPH_ID = "graph1"
 
 user_params = {
     "token": TOKEN,
@@ -18,7 +21,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_params ={
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Walking Graph",
     "unit": "km",
     "type": "float",
@@ -29,5 +32,17 @@ headers ={
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, headers=headers, json=graph_params)
-print(response.text)
+# response = requests.post(url=graph_endpoint, headers=headers, json=graph_params)
+# print(response.text)
+
+pixel_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+
+today = datetime(year=2025, month=5, day=20)
+
+pixel_params = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "20",
+}
+
+# response = requests.post(url=pixel_endpoint, headers=headers, json=pixel_params)
+# print(response.text)

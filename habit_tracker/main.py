c7ea5fcee4c0219e17591a56bf51b440ecb3522b
Rojen Dangol <rojendangol44@gmail.com>
2025-05-21 19:10:32 +0545

    create-user

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,10 +1,12 @@
 import requests
 
 pixela_endpoint = "https://pixe.la/v1/users"
+USERNAME = "rojen123"
+TOKEN = "qwertyuiop"
 
 user_params = {
-    "token": "qwertyuiop",
-    "username": "rojen123",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
@@ -12,3 +14,20 @@ user_params = {
 # create user
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_params ={
+    "id": "graph1",
+    "name": "Walking Graph",
+    "unit": "km",
+    "type": "float",
+    "color": "momiji",
+}
+
+headers ={
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, headers=headers, json=graph_params)
+print(response.text)
